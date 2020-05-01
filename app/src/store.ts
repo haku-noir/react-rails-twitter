ce3e847bc8930be813705ff7d81e517924e3463b
haku-noir <hakunoir.00@gmail.com>
2020-05-01 19:23:50 +0900

    add: storeにツイートを管理するtweetsReducerを追加

--- app/src/store.ts
+++ app/src/store.ts
@@ -4,14 +4,17 @@ import {
 import { connectRouter, routerMiddleware, RouterState } from 'connected-react-router';
 import { History } from 'history';
 import logger from 'redux-logger';
+import { TweetsState, tweetsReducer } from 'reducers/tweetsReducer';
 
 export type RootState = {
   router: RouterState,
+  tweets: TweetsState
 };
 
 export const createStore = (history: History) => {
   const rootReducer = combineReducers<RootState>({
     router: connectRouter(history),
+    tweets: tweetsReducer
   });
 
   return reduxCreateStore(
