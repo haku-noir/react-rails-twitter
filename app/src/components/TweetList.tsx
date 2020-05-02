808ee5edc2360d6463e1d1365afc3e0913f67a43
haku-noir <hakunoir.00@gmail.com>
2020-05-03 01:21:39 +0900

    update: material-uiでTweetListコンポーネントを更新

--- app/src/components/TweetList.tsx
+++ app/src/components/TweetList.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
 import { TweetsState } from 'reducers/tweetsReducer';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 export type TweetListStateAsProps = TweetsState;
 
@@ -7,17 +15,50 @@ export type TweetListDispatchAsProps = {};
 
 type IProps = TweetListStateAsProps & TweetListDispatchAsProps;
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: '36ch',
+      backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+      display: 'inline',
+    },
+  }),
+);
+
 export const TweetList: React.FC<IProps> = (props: IProps) => {
   const { tweets } = props;
+  const classes = useStyles();
 
   return (
-    <div>
-      <h1>TweetList</h1>
-      <ul>
-        {tweets.map(tweet => (
-          <p>{tweet}</p>
-        ))}
-      </ul>
-    </div>
+    <List className={classes.root}>
+      {tweets.map(tweet => (
+        <div>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Anonymous" src="" />
+            </ListItemAvatar>
+            <ListItemText
+              primary={tweet}
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    Anonymous
+                  </Typography>
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </div>
+      ))}
+    </List>
   );
 };
