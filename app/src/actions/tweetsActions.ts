6c2af53ec63069e1d3f1546cbc6f2da81b006dee
haku-noir <hakunoir.00@gmail.com>
2020-05-06 23:00:37 +0900

    add: 非同期でapiから全ツイートを読み込むアクションを追加

--- app/src/actions/tweetsActions.ts
+++ app/src/actions/tweetsActions.ts
@@ -1,7 +1,21 @@
 import actionCreatorFactory from 'typescript-fsa';
+import { asyncFactory } from 'typescript-fsa-redux-thunk';
+import { TweetsState } from 'reducers/tweetsReducer';
+import { fetchPosts } from 'clients/posts';
 
 const actionCreator = actionCreatorFactory();
+const asyncActionCreator = asyncFactory<TweetsState>(actionCreator);
 
 export const tweetsActions = {
-  updateTweets: actionCreator<any>('UPDATE_TWEETS'),
+  updateTweets: actionCreator<TweetsState["tweets"]>('UPDATE_TWEETS'),
+  fetchTweets: asyncActionCreator<void, TweetsState["tweets"]>(
+    'FETCH_TWEETS',
+    () => new Promise(resolve => {
+      fetchPosts()
+        .then((res) => res.json())
+        .then((res) => {
+          resolve(res.posts);
+        })
+    })
+  )
 };
