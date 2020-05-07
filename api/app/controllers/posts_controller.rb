c78dbd49758afcc0448ba3eb7bda6bfab9986824
haku-noir <hakunoir.00@gmail.com>
2020-05-07 13:39:32 +0900

    add: apiに新規ツイートを作成する機能の追加

--- api/app/controllers/posts_controller.rb
+++ api/app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
   def index
     posts = Post.order(created_at: :desc)
-    render json: { posts: posts }
+    render json: { data: posts }
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def create
+    post = Post.new(content: params[:content])
+    if post.save
+      render json: { data: post }
+    else
+      render json: { data: post.errors }
+    end
   end
 end
