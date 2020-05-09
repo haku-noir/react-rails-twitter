d43819fe2968b562e52b77c648fc6b6ff4e68b3e
haku-noir <hakunoir.00@gmail.com>
2020-05-09 21:10:26 +0900

    add: ツイートをクリックするとツイートの詳細が表示される機能を追加

--- app/src/components/TweetList.tsx
+++ app/src/components/TweetList.tsx
@@ -5,11 +5,13 @@ import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import { TweetForm } from 'containers/TweetForm';
+import { Link } from 'react-router-dom';
 
 export type TweetListStateAsProps = TweetsState;
 
 export type TweetListDispatchAsProps = {
-  repeat: () => void
+  repeat: () => void,
+  clickItem: (id: number) => void
 };
 
 type IProps = TweetListStateAsProps & TweetListDispatchAsProps;
@@ -28,7 +30,7 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export const TweetList: React.FC<IProps> = (props: IProps) => {
-  const { tweets, repeat } = props;
+  const { tweets, repeat, clickItem } = props;
   const classes = useStyles();
   setTimeout(() => repeat(), 5000);
 
@@ -37,7 +39,7 @@ export const TweetList: React.FC<IProps> = (props: IProps) => {
       <TweetForm />
       <List className={classes.root}>
         {tweets.map(tweet => (
-          <ListItem key={tweet.id} alignItems="flex-start">
+          <ListItem key={tweet.id} alignItems="flex-start" onClick={() => {clickItem(tweet.id)}}>
             <Tweet tweet={tweet} />
           </ListItem>
         ))}
