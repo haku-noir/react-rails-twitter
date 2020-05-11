38fc9edfa3ff4816df7fd48c00db1d218dc5367e
haku-noir <hakunoir.00@gmail.com>
2020-05-11 19:27:06 +0900

    add: TweetShowにアイコンボタンを追加

--- app/src/components/TweetShow.tsx
+++ app/src/components/TweetShow.tsx
@@ -6,7 +6,11 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export type TweetShowStateAsProps = {
   tweet: TweetState
@@ -20,9 +24,6 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: 500,
-    },
-    avatar: {
-      backgroundColor: red[500],
     },
   }),
 );
@@ -45,6 +46,19 @@ export const TweetShow: React.FC<IProps> = (props: IProps) => {
           {tweet.content}
         </Typography>
       </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="favorite">
+          <FavoriteIcon />
+        </IconButton>
+        <div style={{marginLeft: 'auto'}}>
+          <IconButton aria-label="edit" color="primary">
+            <EditIcon />
+          </IconButton>
+          <IconButton aria-label="delete" color="secondary">
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      </CardActions>
     </Card>
   );
 };
