30086195451851ceece612706cc4d7554f2ad196
haku-noir <hakunoir.00@gmail.com>
2020-05-11 21:18:12 +0900

    add: DeleteButtonコンポーネントを追加

---
+++ app/src/components/DeleteButton.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+export type DeleteButtonStateAsProps = {};
+
+export type DeleteButtonDispatchAsProps = {
+  destroy: (id: number) => void;
+};
+
+type DeleteButtonOwnProps = {
+  id: number;
+};
+
+type IProps = DeleteButtonStateAsProps & DeleteButtonDispatchAsProps & DeleteButtonOwnProps;
+
+export const DeleteButton: React.FC<IProps> = (props: IProps) => {
+  const { destroy, id } = props;
+  return (
+    <IconButton aria-label="destroy" color="secondary" onClick={() => destroy(id)}>
+      <DeleteIcon />
+    </IconButton>
+  );
+};
