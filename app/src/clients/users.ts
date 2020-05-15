0d9db0d937babbb58a75a8ea97e9a2ac92b5b6ce
haku-noir <hakunoir.00@gmail.com>
2020-05-15 14:30:25 +0900

    add: ログイン用のクライアントを追加

--- app/src/clients/users.ts
+++ app/src/clients/users.ts
@@ -13,6 +13,17 @@ export const addUser = (user: PassUserState) => fetch(`${baseURL}/users`, {
   body: JSON.stringify(user)
 });
 
+export const login = (user: PassUserState) => fetch(`${baseURL}/login`, {
+  method: "POST",
+  mode: 'cors',
+  credentials: 'include',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(user)
+});
+
 export const getUser = (id: number) => fetch(`${baseURL}/users/${id}`, {
   method: "GET",
   mode: 'cors',
