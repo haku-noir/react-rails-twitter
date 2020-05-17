79b1fbc1b51da822e727c56d8b1c35a11c18937c
haku-noir <hakunoir.00@gmail.com>
2020-05-17 11:08:31 +0900

    add: apiのusersテーブルにuser_idカラムを追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_055615) do
+ActiveRecord::Schema.define(version: 2020_05_17_020419) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
