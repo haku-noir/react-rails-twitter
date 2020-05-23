0191687c5f6922e2f7f410783f1ba4d5cc74b6e8
haku-noir <hakunoir.00@gmail.com>
2020-05-23 11:52:13 +0900

    add: apiにlike関連の各機能を追加

--- api/app/controllers/likes_controller.rb
+++ api/app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user
+
+  def create
+    @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
+    @like.save
+    render json: { like: @like }
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
+    @like.destroy
+    render json: {}
+  end
+
 end
