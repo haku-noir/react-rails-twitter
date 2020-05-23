4a1782ab64646fdd9e85e034f7eae49a9ccb8126
haku-noir <hakunoir.00@gmail.com>
2020-05-23 13:18:24 +0900

    change: apiのlike関連をresourcesからルーティングに変更

--- api/app/controllers/likes_controller.rb
+++ api/app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   before_action :authenticate_user
 
-  def create
+  def like
     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
     @like.save
     render json: { like: @like }
   end
 
-  def destroy
+  def dislike
     @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
     @like.destroy
     render json: {}
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   resources :users
-  resources :likes
   post "login" => "users#login"
   post "logout" => "users#logout"
   get "session" => "users#session_user"
+  post "like" => "like#like"
+  post "dislike" => "like#dislike"
 end
