563464d56e00ccd80a182bb7884ed42868bc1a69
haku-noir <hakunoir.00@gmail.com>
2020-05-26 11:58:02 +0900

    add: follow関連のクライアントを追加

---
+++ app/src/clients/follows.ts
@@ -0,0 +1,23 @@
+const baseURL = 'http://localhost';
+
+export const follow = (follower_id: number, following_user_id: number) => fetch(`${baseURL}/follow`, {
+  method: "POST",
+  mode: 'cors',
+  credentials: 'include',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({follower_id, following_user_id})
+});
+
+export const unfollow = (follower_id: number, following_user_id: number) => fetch(`${baseURL}/unfollow`, {
+  method: "POST",
+  mode: 'cors',
+  credentials: 'include',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({follower_id, following_user_id})
+});
